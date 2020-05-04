8951e7775f78a2bbbbba38d676af18f4cb9f66ea
bastienmp <bastien.mpp@gmail.com>
2020-05-04 23:05:11 +0200

    Update Test.cs
    remove button listener to use editor functionnality

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -13,25 +13,8 @@ public class Test : MonoBehaviour
     void Start()
     {
         buttonEventParameterless.interactable = false;
-        buttonEventParameterless.onClick.AddListener(() => {
-            _manager.SendCustomEvent("Event_parameterless");
-        });
-
         buttonEventParameters.interactable = false;
-        buttonEventParameters.onClick.AddListener(() => {
-            Dictionary<string, object> eventParams = new Dictionary<string, object>();
-            eventParams["valeur"] = 42;
-            _manager.SendCustomEvent("Event_params", eventParams);
-        });
-
         buttonEventFull.interactable = false;
-        buttonEventFull.onClick.AddListener(() => {
-            Dictionary<string, object> eventParams = new Dictionary<string, object>();
-            eventParams["nom"] = "toto";
-            eventParams["value"] = 12;
-
-            _manager.SendCustomEvent("Event_full", eventParams);
-        });
 
         _manager.Initialize(OnServicesInitialized);
     }
@@ -41,5 +24,26 @@ public class Test : MonoBehaviour
         buttonEventParameterless.interactable = true;
         buttonEventParameters.interactable = true;
         buttonEventFull.interactable = true;
+    }
+
+    public void TriggerEventParameterless()
+    {
+        _manager.SendCustomEvent("Event_parameterless");
+    }
+
+    public void TriggerEventParams()
+    {
+        Dictionary<string, object> eventParams = new Dictionary<string, object>();
+        eventParams["valeur"] = 42;
+        _manager.SendCustomEvent("Event_params", eventParams);
+    }
+
+    public void TriggerEventFull()
+    {
+        Dictionary<string, object> eventParams = new Dictionary<string, object>();
+        eventParams["nom"] = "toto";
+        eventParams["value"] = 12;
+
+        _manager.SendCustomEvent("Event_full", eventParams);
     }
 }
